1ad9b957b45df34773fd31e485660d4e8ea68536
滚雪球 <42798930+siknight@users.noreply.github.com>
2020-10-18 17:43:15 +0800

    Update aaa.java

--- src/aaa.java
+++ src/aaa.java
@@ -1,6 +1,7 @@
 public class aaa {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(111222);
         System.out.println("DEV");
+        int  q=1;
     }
 }
