ae08b0244ead62fa2dff50051d4e632e942af278
lisi <1786678583@qq.com>
2020-10-18 17:45:59 +0800

    1

--- src/aaa.java
+++ src/aaa.java
@@ -1,7 +1,9 @@
 public class aaa {
     public static void main(String[] args) {
         System.out.println(111222);
+        System.out.println(2222);
         System.out.println("DEV");
-        int  q=1;
+        System.out.println("jloianolaj");
+        int c=222;
     }
 }
